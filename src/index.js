b7c2f61968423b75de5e2e4b04f456368b8e7498
yangxiooping <yangxiaoping@jd.com>
2017-09-05 18:53:32 +0800

    React和ES6以及JSX语法的支持

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@
 * @Author: yangxiooping
 * @Date:   2017-09-05 17:04:30
 * @Last Modified by:   yangxiooping
-* @Last Modified time: 2017-09-05 17:08:56
+* @Last Modified time: 2017-09-05 18:39:53
 */
 
-function component() {
-    var element = document.createElement('div');
-    element.innerHTML = 'Hello, webpack';
-    return element;
+import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+
+class Hello extends Component{
+  render() {
+    return (
+      <div>
+        Hello, webpack
+      </div>
+    );
+  }
 }
-document.body.appendChild(component());
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,26 @@
 * @Author: yangxiooping
 * @Date:   2017-09-05 17:18:22
 * @Last Modified by:   yangxiooping
-* @Last Modified time: 2017-09-05 17:28:52
+* @Last Modified time: 2017-09-05 18:39:06
 */
 
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
+  	entry: './src/index.js',
+  	output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    	path: path.resolve(__dirname, 'dist')
+  	},
+  	module: {
+	    rules: [
+	        {
+	            test: /(\.jsx|\.js)$/,
+	            use: {
+	                loader: "babel-loader"
+	            },
+	            exclude: /node_modules/
+	        }
+	    ]
+	}
 };
