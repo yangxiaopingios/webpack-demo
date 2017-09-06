d9598f98900f4919af6b63b5c4697514727db841
yangxiooping <yangxiaoping@jd.com>
2017-09-06 10:53:51 +0800

    加载css

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@
 * @Author: yangxiooping
 * @Date:   2017-09-05 17:04:30
 * @Last Modified by:   yangxiooping
-* @Last Modified time: 2017-09-05 18:39:53
+* @Last Modified time: 2017-09-06 10:46:06
 */
 
 import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
+import {hello} from './style.css';
 
 class Hello extends Component{
   render() {
     return (
-      <div>
+      <div className={hello}>
         Hello, webpack
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
 /*
-* @Author: yangxiooping
-* @Date:   2017-09-05 17:18:22
-* @Last Modified by:   yangxiooping
-* @Last Modified time: 2017-09-05 18:39:06
-*/
+ * @Author: yangxiooping
+ * @Date:   2017-09-05 17:18:22
+ * @Last Modified by:   yangxiooping
+ * @Last Modified time: 2017-09-06 10:37:34
+ */
 
 const path = require('path');
 
 module.exports = {
-  	entry: './src/index.js',
-  	output: {
-    filename: 'bundle.js',
-    	path: path.resolve(__dirname, 'dist')
-  	},
-  	module: {
-	    rules: [
-	        {
-	            test: /(\.jsx|\.js)$/,
-	            use: {
-	                loader: "babel-loader"
-	            },
-	            exclude: /node_modules/
-	        }
-	    ]
-	}
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader",
+                     options: {
+                        modules: true,
+                        minimize: true
+                     }
+                }]
+            }
+        ]
+    }
 };
