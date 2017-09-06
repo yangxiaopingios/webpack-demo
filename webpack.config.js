9422c9222c2bd9e4e498c17214c89c42037c0d51
yangxiooping <yangxiaoping@jd.com>
2017-09-06 11:34:34 +0800

    HtmlWebpackPlugin 插件

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@
  * @Author: yangxiooping
  * @Date:   2017-09-05 17:18:22
  * @Last Modified by:   yangxiooping
- * @Last Modified time: 2017-09-06 10:55:20
+ * @Last Modified time: 2017-09-06 11:28:25
  */
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -40,5 +41,16 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/index.tmpl.html",//new 一个这个插件的实例，并传入相关的参数
+            minify: {
+                removeComments: true, //是否去掉注释
+                collapseWhitespace: true, //是否去掉空格
+                minifyJS: true, //是否压缩html里的js（使用uglify-js进行的压缩）
+                minifyCSS: true, //是否压缩html里的css（使用clean-css进行的压缩）
+            },
+        })
+    ],
 };
